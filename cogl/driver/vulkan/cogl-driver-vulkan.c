cb7b30aed4495b6c05feeb297a8dc1fd6ef9c054
Lionel Landwerlin <llandwerlin@gmail.com>
2016-06-12 23:38:49 +0200

    vulkan: driver: support pbos to allow copies of framebuffers

--- cogl/driver/vulkan/cogl-driver-vulkan.c
+++ cogl/driver/vulkan/cogl-driver-vulkan.c
@@ -103,6 +103,7 @@ _cogl_driver_update_features (CoglContext *ctx,
                                             NULL);
 
   COGL_FLAGS_SET (ctx->private_features, COGL_PRIVATE_FEATURE_OFFSCREEN_BLIT, TRUE);
+  COGL_FLAGS_SET (ctx->private_features, COGL_PRIVATE_FEATURE_PBOS, TRUE);
   COGL_FLAGS_SET (ctx->private_features, COGL_PRIVATE_FEATURE_VBOS, TRUE);
   COGL_FLAGS_SET (ctx->features, COGL_FEATURE_ID_GLSL, TRUE);
   COGL_FLAGS_SET (ctx->features, COGL_FEATURE_ID_OFFSCREEN, TRUE);
