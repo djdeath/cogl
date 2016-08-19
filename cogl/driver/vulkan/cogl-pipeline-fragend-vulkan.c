477dc3dc6932433958165a8ceeb00514f72aff09
Lionel Landwerlin <llandwerlin@gmail.com>
2016-08-19 16:16:53 +0100

    vulkan: fragend: coding style

--- cogl/driver/vulkan/cogl-pipeline-fragend-vulkan.c
+++ cogl/driver/vulkan/cogl-pipeline-fragend-vulkan.c
@@ -469,7 +469,7 @@ ensure_texture_lookup_generated (CoglPipelineShaderState *shader_state,
                          "vec4(1.0, 1.0, 1.0, 1.0);\n");
       else
         g_string_append_printf (shader_state->header,
-                                "texture(tex, coords.%s);\n",
+                                "texture (tex, coords.%s);\n",
                                 tex_coord_swizzle);
 
       g_string_append (shader_state->header, "}\n");
