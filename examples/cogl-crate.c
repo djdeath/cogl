e2f7330812b90f5aa731dea144bb4c7519daeb3f
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2016-10-25 18:32:54 +0100

    custom test

--- examples/cogl-crate.c
+++ examples/cogl-crate.c
@@ -1,9 +1,15 @@
 #include <cogl/cogl.h>
 #include <cogl-pango/cogl-pango.h>
 
+#include <assert.h>
+
 /* The state for this example... */
 typedef struct _Data
 {
+  CoglFramebuffer *off_fb;
+  int off_fb_width;
+  int off_fb_height;
+
   CoglFramebuffer *fb;
   int framebuffer_width;
   int framebuffer_height;
@@ -12,27 +18,14 @@ typedef struct _Data
 
   CoglIndices *indices;
   CoglPrimitive *prim;
-  CoglTexture *texture;
   CoglPipeline *crate_pipeline;
 
-  CoglPangoFontMap *pango_font_map;
-  PangoContext *pango_context;
-  PangoFontDescription *pango_font_desc;
-
-  PangoLayout *hello_label;
-  int hello_label_width;
-  int hello_label_height;
+  CoglPipeline *copy_pipeline;
 
   GTimer *timer;
 
   CoglBool swap_ready;
-
 } Data;
-
-/* A static identity matrix initialized for convenience. */
-static CoglMatrix identity;
-/* static colors initialized for convenience. */
-static CoglColor white;
 
 /* A cube modelled using 4 vertices for each face.
  *
@@ -79,28 +72,23 @@ static CoglVertexP3T2 vertices[] =
 };
 
 static void
-paint (Data *data)
+paint_cube (Data *data)
 {
-  CoglFramebuffer *fb = data->fb;
-  float rotation;
+  CoglFramebuffer *fb = data->off_fb;
+  float rotation= 45;
 
   cogl_framebuffer_clear4f (fb,
                             COGL_BUFFER_BIT_COLOR|COGL_BUFFER_BIT_DEPTH,
                             0, 0, 0, 1);
-
+  
   cogl_framebuffer_push_matrix (fb);
 
   cogl_framebuffer_translate (fb,
-                              data->framebuffer_width / 2,
-                              data->framebuffer_height / 2,
+                              cogl_framebuffer_get_width (fb) / 2,
+                              cogl_framebuffer_get_height (fb) / 2,
                               0);
 
-  cogl_framebuffer_scale (fb, 75, 75, 75);
-
-  /* Update the rotation based on the time the application has been
-     running so that we get a linear animation regardless of the frame
-     rate */
-  rotation = g_timer_elapsed (data->timer, NULL) * 60.0f;
+  cogl_framebuffer_scale (fb, 300, 300, 300);
 
   /* Rotate the cube separately around each axis.
    *
@@ -118,22 +106,28 @@ paint (Data *data)
   cogl_primitive_draw (data->prim, fb, data->crate_pipeline);
 
   cogl_framebuffer_pop_matrix (fb);
+}
 
-  /* And finally render our Pango layouts... */
+static void
+paint_fb (Data *data)
+{
+  CoglFramebuffer *fb = data->fb;
 
-  cogl_pango_show_layout (fb, data->hello_label,
-                          (data->framebuffer_width / 2) -
-                          (data->hello_label_width / 2),
-                          (data->framebuffer_height / 2) -
-                          (data->hello_label_height / 2),
-                          &white);
+  cogl_framebuffer_clear4f (fb,
+                            COGL_BUFFER_BIT_COLOR|COGL_BUFFER_BIT_DEPTH,
+                            0, 0, 0, 1);
+
+  cogl_framebuffer_draw_rectangle (fb, data->copy_pipeline,
+				   0, 0,
+				   cogl_framebuffer_get_width (fb),
+				   cogl_framebuffer_get_height (fb));
 }
 
 static void
 frame_event_cb (CoglOnscreen *onscreen,
-                CoglFrameEvent event,
-                CoglFrameInfo *info,
-                void *user_data)
+		CoglFrameEvent event,
+		CoglFrameInfo *info,
+		void *user_data)
 {
   Data *data = user_data;
 
@@ -141,17 +135,73 @@ frame_event_cb (CoglOnscreen *onscreen,
     data->swap_ready = TRUE;
 }
 
+static void
+draw_onscreen (CoglContext *ctx, Data *data)
+{
+  data->swap_ready = TRUE;
+
+  paint_cube (data);
+  
+  cogl_onscreen_add_frame_callback (COGL_ONSCREEN (data->fb),
+				    frame_event_cb,
+				    &data,
+				    NULL);
+  while (1)
+    {
+      CoglPollFD *poll_fds;
+      int n_poll_fds;
+      int64_t timeout;
+      
+      if (data->swap_ready)
+	{
+	  paint_fb (data);
+	  cogl_onscreen_swap_buffers (COGL_ONSCREEN (data->fb));
+	}
+      
+      cogl_poll_renderer_get_info (cogl_context_get_renderer (ctx),
+				   &poll_fds, &n_poll_fds, &timeout);
+      
+      g_poll ((GPollFD *) poll_fds, n_poll_fds,
+	      timeout == -1 ? -1 : timeout / 1000);
+      
+      cogl_poll_renderer_dispatch (cogl_context_get_renderer (ctx),
+				   poll_fds, n_poll_fds);
+    }
+}
+
+static void
+draw_offscreen (Data *data)
+{
+  uint32_t i;
+
+  paint_cube (data);
+
+  data->timer = g_timer_new ();
+  g_timer_start (data->timer);
+  
+  for (i = 0; i < 10000; i++) {
+    paint_fb (data);
+  }
+  cogl_framebuffer_finish (data->fb);
+  
+  g_timer_stop (data->timer);
+
+  printf ("elapsed=%f\n", g_timer_elapsed (data->timer, NULL));
+}
+
 int
 main (int argc, char **argv)
 {
   CoglContext *ctx;
-  CoglOnscreen *onscreen;
-  CoglFramebuffer *fb;
+  CoglTexture *offscreen_texture, *depth_texture;
+  CoglOffscreen *offscreen;
   CoglError *error = NULL;
   Data data;
-  PangoRectangle hello_label_size;
   float fovy, aspect, z_near, z_2d, z_far;
   CoglDepthState depth_state;
+  uint8_t *pixels;
+  uint32_t width = 1920, height = 1080;
+  CoglBool use_offscreen = TRUE;
 
   ctx = cogl_context_new (NULL, &error);
   if (!ctx) {
@@ -159,19 +209,60 @@ main (int argc, char **argv)
       return 1;
   }
 
-  onscreen = cogl_onscreen_new (ctx, 640, 480);
-  fb = onscreen;
-  data.fb = fb;
-  data.framebuffer_width = cogl_framebuffer_get_width (fb);
-  data.framebuffer_height = cogl_framebuffer_get_height (fb);
+  offscreen_texture = cogl_texture_2d_new_with_size (ctx, 1920, 1080);
+  offscreen = cogl_offscreen_new_with_texture (offscreen_texture);
+  cogl_object_unref (offscreen_texture);
+  data.off_fb = offscreen;
+  cogl_framebuffer_set_depth_texture_enabled (data.off_fb, TRUE);
+  cogl_framebuffer_set_depth_write_enabled (data.off_fb, TRUE);
+  data.off_fb_width = cogl_framebuffer_get_width (data.off_fb);
+  data.off_fb_height = cogl_framebuffer_get_height (data.off_fb);
+  cogl_framebuffer_orthographic (data.off_fb, 0, 0,
+				 data.off_fb_width, data.off_fb_height,
+				 -1, 100);
 
-  data.timer = g_timer_new ();
+  assert (cogl_framebuffer_allocate (data.off_fb, NULL));
 
-  cogl_onscreen_show (onscreen);
+  depth_texture = cogl_framebuffer_get_depth_texture (data.off_fb);
+  
+  /**/
+  if (use_offscreen)
+    {
+      offscreen_texture = cogl_texture_2d_new_with_size (ctx, width, height);
+      offscreen = cogl_offscreen_new_with_texture (offscreen_texture);
+      cogl_object_unref (offscreen_texture);
+      data.fb = offscreen;
+    }
+  else
+    {
+      data.fb = cogl_onscreen_new (ctx, width, height);
+    }
 
-  cogl_framebuffer_set_viewport (fb, 0, 0,
+  if (!cogl_is_onscreen (data.fb))
+    {
+      cogl_framebuffer_set_depth_texture_enabled (data.fb, FALSE);
+      cogl_framebuffer_set_depth_write_enabled (data.fb, FALSE);
+    }
+  else
+    {
+      //cogl_framebuffer_set_samples_per_pixel (data.fb, 4);
+    }
+  data.framebuffer_width = cogl_framebuffer_get_width (data.fb);
+  data.framebuffer_height = cogl_framebuffer_get_height (data.fb);
+  cogl_framebuffer_orthographic (data.fb, 0, 0,
+				 data.framebuffer_width, data.framebuffer_height,
+				 -1, 100);
+
+  assert (cogl_framebuffer_allocate (data.fb, NULL));
+  if (cogl_is_onscreen (data.fb))
+    cogl_onscreen_show (COGL_ONSCREEN (data.fb));
+
+  cogl_framebuffer_set_viewport (data.fb, 0, 0,
                                  data.framebuffer_width,
                                  data.framebuffer_height);
+  cogl_framebuffer_set_viewport (data.off_fb, 0, 0,
+                                 data.off_fb_width,
+                                 data.off_fb_height);
 
   fovy = 60; /* y-axis field of view */
   aspect = (float)data.framebuffer_width/(float)data.framebuffer_height;
@@ -179,7 +270,8 @@ main (int argc, char **argv)
   z_2d = 1000; /* position to 2d plane */
   z_far = 2000; /* distance to far clipping plane */
 
-  cogl_framebuffer_perspective (fb, fovy, aspect, z_near, z_far);
+  cogl_framebuffer_perspective (data.fb, fovy, aspect, z_near, z_far);
+  cogl_framebuffer_perspective (data.off_fb, fovy, aspect, z_near, z_far);
 
   /* Since the pango renderer emits geometry in pixel/device coordinates
    * and the anti aliasing is implemented with the assumption that the
@@ -196,11 +288,12 @@ main (int argc, char **argv)
   cogl_matrix_view_2d_in_perspective (&data.view, fovy, aspect, z_near, z_2d,
                                       data.framebuffer_width,
                                       data.framebuffer_height);
-  cogl_framebuffer_set_modelview_matrix (fb, &data.view);
-
-  /* Initialize some convenient constants */
-  cogl_matrix_init_identity (&identity);
-  cogl_color_init_from_4ub (&white, 0xff, 0xff, 0xff, 0xff);
+  cogl_framebuffer_set_modelview_matrix (data.fb, &data.view);
+  cogl_matrix_init_identity (&data.view);
+  cogl_matrix_view_2d_in_perspective (&data.view, fovy, aspect, z_near, z_2d,
+                                      data.off_fb_width,
+                                      data.off_fb_height);
+  cogl_framebuffer_set_modelview_matrix (data.off_fb, &data.view);
 
   /* rectangle indices allow the GPU to interpret a list of quads (the
    * faces of our cube) as a list of triangles.
@@ -218,21 +311,12 @@ main (int argc, char **argv)
                               data.indices,
                               6 * 6);
 
-  /* Load a jpeg crate texture from a file */
-  printf ("crate.jpg (CC by-nc-nd http://bit.ly/9kP45T) ShadowRunner27 http://bit.ly/m1YXLh\n");
-  data.texture =
-    cogl_texture_2d_new_from_file (ctx,
-                                   COGL_EXAMPLES_DATA "crate.jpg",
-                                   &error);
-  if (!data.texture)
-    g_error ("Failed to load texture: %s", error->message);
-
   /* a CoglPipeline conceptually describes all the state for vertex
    * processing, fragment processing and blending geometry. When
    * drawing the geometry for the crate this pipeline says to sample a
    * single texture during fragment processing... */
   data.crate_pipeline = cogl_pipeline_new (ctx);
-  cogl_pipeline_set_layer_texture (data.crate_pipeline, 0, data.texture);
+  cogl_pipeline_set_color4f (data.crate_pipeline, 1.0, 0.0, 0.0, 1.0);
 
   /* Since the box is made of multiple triangles that will overlap
    * when drawn and we don't control the order they are drawn in, we
@@ -240,60 +324,35 @@ main (int argc, char **argv)
    * be visible get culled by the GPU. */
   cogl_depth_state_init (&depth_state);
   cogl_depth_state_set_test_enabled (&depth_state, TRUE);
-
   cogl_pipeline_set_depth_state (data.crate_pipeline, &depth_state, NULL);
 
-  /* Setup a Pango font map and context */
+  data.copy_pipeline = cogl_pipeline_new (ctx);
+  cogl_pipeline_set_layer_texture (data.copy_pipeline, 0, depth_texture);
 
-  data.pango_font_map = COGL_PANGO_FONT_MAP (cogl_pango_font_map_new());
-
-  cogl_pango_font_map_set_use_mipmapping (data.pango_font_map, TRUE);
-
-  data.pango_context = cogl_pango_font_map_create_context (data.pango_font_map);
-
-  data.pango_font_desc = pango_font_description_new ();
-  pango_font_description_set_family (data.pango_font_desc, "Sans");
-  pango_font_description_set_size (data.pango_font_desc, 30 * PANGO_SCALE);
-
-  /* Setup the "Hello Cogl" text */
-
-  data.hello_label = pango_layout_new (data.pango_context);
-  pango_layout_set_font_description (data.hello_label, data.pango_font_desc);
-  pango_layout_set_text (data.hello_label, "Hello Cogl", -1);
-
-  pango_layout_get_extents (data.hello_label, NULL, &hello_label_size);
-  data.hello_label_width = PANGO_PIXELS (hello_label_size.width);
-  data.hello_label_height = PANGO_PIXELS (hello_label_size.height);
-
-  data.swap_ready = TRUE;
-
-  cogl_onscreen_add_frame_callback (COGL_ONSCREEN (fb),
-                                    frame_event_cb,
-                                    &data,
-                                    NULL); /* destroy notify */
+  cogl_pipeline_add_layer_snippet (data.copy_pipeline, 0,
+  				   cogl_snippet_new (COGL_SNIPPET_HOOK_LAYER_FRAGMENT,
+  						     "float linearize_depth(float depth) {\n"
+						     "  float n = 0.1;\n"
+						     "  float f = 1000.0;\n"
+						     "  return (2.0 * n) / (f + n - depth * (f - n));\n"
+						     "}\n",
+  						     "  vec4 depth = texture2D (cogl_sampler0, cogl_tex_coord0_in.xy);\n"
+  						     "  cogl_layer = vec4(1.0 - linearize_depth(depth.r), 0, 0, 1.0);\n"));
 
 
-  while (1)
-    {
-      CoglPollFD *poll_fds;
-      int n_poll_fds;
-      int64_t timeout;
+  if (cogl_is_onscreen (data.fb))
+    draw_onscreen (ctx, &data);
+  else
+    draw_offscreen (&data);
 
-      if (data.swap_ready)
-        {
-          paint (&data);
-          cogl_onscreen_swap_buffers (COGL_ONSCREEN (fb));
-        }
+  pixels = g_malloc (data.framebuffer_width * data.framebuffer_height * 4);
+  cogl_framebuffer_read_pixels (data.fb, 0, 0,
+  				data.framebuffer_width,
+  				data.framebuffer_height,
+  				COGL_PIXEL_FORMAT_RGBA_8888, pixels);
 
-      cogl_poll_renderer_get_info (cogl_context_get_renderer (ctx),
-                                   &poll_fds, &n_poll_fds, &timeout);
-
-      g_poll ((GPollFD *) poll_fds, n_poll_fds,
-              timeout == -1 ? -1 : timeout / 1000);
-
-      cogl_poll_renderer_dispatch (cogl_context_get_renderer (ctx),
-                                   poll_fds, n_poll_fds);
-    }
+  g_file_set_contents ("/tmp/bmp", (const char *) pixels,
+  		       data.framebuffer_width * data.framebuffer_height * 4, NULL);
 
   return 0;
 }
